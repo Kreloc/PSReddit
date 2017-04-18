0e13bd07842c2f1e4fddc25d47e5603be051e2c4
Kreloc <kingpincat@hotmail.com>
2017-04-18 12:38:05 -0800

    Added Get-RedditSubListing function

---
+++ Public/Get-RedditSubListing.ps1
@@ -0,0 +1,38 @@
+function Get-RedditSubListing {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=1, Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [Alias("r","Subreddit")]
+        [string[]]
+        $Name = 'all',
+        [Parameter(
+            Position = 0,
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true
+            )]
+        [Alias("Link")]
+        $accessToken=$Global:PSReddit_accessToken        
+    )
+    
+    begin {
+    }
+    
+    process {
+        
+        foreach ($sub in $Name) {
+            $uri = "https://oAuth.reddit.com/r/$Name/new"
+            Write-Verbose "Sending a uri of $($uri)"
+            $response = (Invoke-RestMethod $uri -Headers @{"Authorization" = "bearer $accessToken"} -ErrorAction STOP)
+            # TODO: Add formatting instead of piping to Select Object
+            $response.data.children | ForEach-Object {
+                $_.data | Select title, selfText, id, score, author, permalink, url, created_utc, num_comments, ups, downs
+            }
+
+            # $test.data.children[0].data
+            # selfText, id, score, author, permalink, url, created_utc, num_comments, ups, downs
+        }
+    }
+    
+    end {
+    }
+}
