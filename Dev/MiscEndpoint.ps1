d9a9ecfdb98149d679ee65e8a6827f8ee3d11a29
Kreloc <kingpincat@hotmail.com>
2017-04-19 16:23:32 -0800

    Added two more endpoint files These are just notes about what to develop

---
+++ Dev/MiscEndpoint.ps1
@@ -0,0 +1,6 @@
+# GET /api/v1/scopes
+# Retrieve descriptions of reddit's OAuth2 scopes.
+# If no scopes are given, information on all scopes are returned.
+# Invalid scope(s) will result in a 400 error with body that indicates the invalid scope(s).
+# scopes	
+# (optional) An OAuth2 scope string
